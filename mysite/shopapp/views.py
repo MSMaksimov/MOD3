8366e96f9b99c29ab57303c0c79052d50fec0cf9
Максим Максимов <mmax77@mail.ru>
2023-03-23 13:03:18 +0500

    Models add

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,9 +1,26 @@
+from timeit import default_timer
+
+from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 
 def shop_index(request: HttpRequest):
-    print(request.path)
-    print(request.method)
-    print(request.headers)
-    return HttpResponse("Hello World!")
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop_index.html', context=context)
+
+
+def groups_list(request: HttpRequest):
+    context = {
+        "groups": Group.objects.prefetch_related('permissions').all(),
+    }
+    return render(request, 'shopapp/groups-list.html', context=context)
