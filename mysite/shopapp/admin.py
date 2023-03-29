432239b832cf82d471ac7dc3c0625c3816cbbf84
Максим Максимов <mmax77@mail.ru>
2023-03-29 14:03:39 +0500

    pause

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Order
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     # list_display = "pk", "name", "description", "price", "discount"
     list_display = "pk", "name", "description_short", "price", "discount"
-
     list_display_links = "pk", "name"
+    ordering = "-name", "pk"
+    search_fields = "name", "description"
 
     def description_short(self, obj: Product) -> str:
         if len(obj.description) < 48:
@@ -16,3 +17,22 @@ class ProductAdmin(admin.ModelAdmin):
         return obj.description[:48] + "..."
 
 # admin.site.register(Product, ProductAdmin)
+
+# class ProductInline(admin.TabularInline):
+
+    class ProductInline(admin.StackedInline):
+        model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+    list_display = "delivery_address", "promocode", "created_at", "user_verbose"
+
+    def get_queryset(self, request):
+        return Order.objects.select_related("user").prefetch_related("products")
+
+    def user_verbose(self, obj: Order) -> str:
+        return obj.user.first_name or obj.user.username
