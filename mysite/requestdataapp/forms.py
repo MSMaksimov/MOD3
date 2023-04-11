adac783a531973376b193c3c617e61cc2f08471d
Максим Максимов <mmax77@mail.ru>
2023-04-11 14:40:27 +0500

    Add func validate_file_name

--- mysite/requestdataapp/forms.py
+++ mysite/requestdataapp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.exceptions import ValidationError
 
 
 class UserBioForm(forms.Form):
@@ -6,3 +8,11 @@ class UserBioForm(forms.Form):
     age = forms.IntegerField(label="Your age", min_value=1, max_value=99)
     bio = forms.CharField(label="Biography", widget=forms.Textarea)
 
+
+def validate_file_name(file: InMemoryUploadedFile) -> None:
+    if file.name and "virus" in file.name:
+        raise ValidationError("File name should not contain 'virus")
+
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField(validators=[validate_file_name])
