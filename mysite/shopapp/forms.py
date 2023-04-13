5ca35bbe547dcc73c1995fc5e628bc2e2d5e3dab
Максим Максимов <mmax77@mail.ru>
2023-04-13 22:13:46 +0500

    add create group form

--- mysite/shopapp/forms.py
+++ mysite/shopapp/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.core import validators
+from django.forms import ModelForm
+from django.contrib.auth.models import Group
 
 from .models import Product
 
@@ -8,3 +10,9 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "name", "price", "description", "discount"
+
+
+class GroupForm(ModelForm):
+    class Meta:
+        model = Group
+        fields = ["name"]
