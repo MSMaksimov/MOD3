adaedabd480f8d54010068c20a0cea1f79c16018
Максим Максимов <mmax77@mail.ru>
2023-04-21 10:42:20 +0500

    Pause

--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -3,24 +3,27 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView
 
 
-def login_view(request: HttpRequest) -> HttpResponse:
-    if request.method == "GET":
-        if request.user.is_authenticated:
-            return redirect('/admin/')
-
-        return render(request, 'myauth/login.html/')
-
-    username = request.POST["username"]
-    password = request.POST["password"]
-
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect("/admin/")
-
-    return render(request, "myauth/login.html", {"error": "Invalid login credentials"})
+class AboutMyView(TemplateView):
+    pass
+# def login_view(request: HttpRequest) -> HttpResponse:
+#     if request.method == "GET":
+#         if request.user.is_authenticated:
+#             return redirect('/admin/')
+#
+#         return render(request, 'myauth/login.html/')
+#
+#     username = request.POST["username"]
+#     password = request.POST["password"]
+#
+#     user = authenticate(request, username=username, password=password)
+#     if user is not None:
+#         login(request, user)
+#         return redirect("/admin/")
+#
+#     return render(request, "myauth/login.html", {"error": "Invalid login credentials"})
 
 
 # def logout_view(request: HttpRequest):
