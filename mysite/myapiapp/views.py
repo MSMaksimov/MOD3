f89c993cb5474944a13d5dfd5b0055a21a80bc47
Максим Максимов <mmax77@mail.ru>
2023-06-03 21:51:33 +0500

    Added hello_world_view func

--- mysite/myapiapp/views.py
+++ mysite/myapiapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
 
-# Create your views here.
+
+@api_view()
+def hello_world_view(request: Request) -> Response:
+    return Response({"message": "Hello World!"})
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('shop/', include('shopapp.urls')),
     # path('req/', include('requestdataapp.urls')),
+    path('api/', include('myapiapp.urls')),
 ]
 
 urlpatterns += i18n_patterns(
