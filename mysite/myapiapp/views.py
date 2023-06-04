3f8c1c8892e711953a4310801b529b1f69e85121
Максим Максимов <mmax77@mail.ru>
2023-06-04 07:45:48 +0500

    Added class GroupsListView(APIView)

--- mysite/myapiapp/views.py
+++ mysite/myapiapp/views.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.models import Group
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.views import APIView
 
 
 @api_view()
 def hello_world_view(request: Request) -> Response:
     return Response({"message": "Hello World!"})
+
+
+class GroupsListView(APIView):
+    def get(self, request: Request) -> Response:
+        groups = Group.objects.all()
+        data = [group.name for group in groups]
+        return Response({"groups": data})
