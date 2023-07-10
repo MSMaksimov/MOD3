ca810d293505ca728bfc6798a6288d44b6c7d1c5
Максим Максимов <mmax77@mail.ru>
2023-07-10 16:51:02 +0200

    Too many changed

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,7 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,31 +17,33 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
+from django.contrib.sitemaps.views import sitemap
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from .sitemaps import sitemaps
+
 urlpatterns = [
-    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-    # path('shop/', include('shopapp.urls')),
-    # path('req/', include('requestdataapp.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
-    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name="swagger"),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name="redoc"),
-    path('api/', include('myapiapp.urls')),
-]
-
-urlpatterns += i18n_patterns(
-    path('myauth/', include('myauth.urls')),
     path('shop/', include('shopapp.urls')),
-)
+    path('myauth/', include('myauth.urls')),
+    path('blog/', include('blogapp.urls')),
+
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    )
+]
 
 if settings.DEBUG:
     urlpatterns.extend(
         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     )
 
-if settings.DEBUG:
     urlpatterns.extend(
         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    )
+
+    urlpatterns.append(
+        path('__debug__/', include('debug_toolbar.urls')),
     )
